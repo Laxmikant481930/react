00b9c0fde8d7dd7381b01e44103694dec796da28
Laxmikant481930 <laxmikantdhawade08@gmail.com>
2024-04-21 18:54:47 +0530

    todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
 
 function App() {
+  // const item = ["item 1", "item 2", "item 3", "item 4", "item 5"]
+  const [items, setItems] = useState([
+    "Item 1",
+    "Item 2",
+    "Item 3",
+    "Item 4",
+    "Item 5",
+  ]);
+  const [value, setValue] = useState("")  // value to update from user & setvalue use to update html
+
+  function onvaluechange(e) {
+  //  console.log(e.target.value)
+    setValue(e.target.value)
+  }
+  function onAdd() {
+    console.log(value);
+
+    const newItems=[...items,value];
+    setItems(newItems);
+    setValue("");
+
+  }
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ToDo list </h1>
+
+
+        <p> hello world</p>
+
+        <input type="text" placeholder="enter todo item :"
+          value={value}
+          onChange={onvaluechange}
+        />
+        <button onClick={onAdd}>Add</button>
+
+
+        <ol>
+          {items.map((item) => (
+            <li style={{
+              color: "red",
+            }}
+            >
+              {item} </li>
+          ))}
+        </ol>
       </header>
     </div>
+
+
+
   );
+
+
+
+
 }
 
 export default App;
